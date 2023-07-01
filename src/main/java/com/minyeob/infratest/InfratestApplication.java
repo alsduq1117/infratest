edcdabeb5c19a6f2d3a2a76187e9623931d29e44
alsduq1117 <alsduq1117@naver.com>
2023-07-01 19:12:50 +0900

    Initial commit

---
+++ src/main/java/com/minyeob/infratest/InfratestApplication.java
@@ -0,0 +1,13 @@
+package com.minyeob.infratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfratestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfratestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minyeob/infratest/InfratestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minyeob.infratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfratestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
